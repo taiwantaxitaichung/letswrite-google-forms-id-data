e2d569e1761cd1a33959ebf40c0a8c32f47867b1
taiwantaxitaichung <99558005+taiwantaxitaichung@users.noreply.github.com>
2022-02-14 14:02:48 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,26 +14,26 @@ const FormAutoFill = new Vue({
     person: {},
 
     // Google Form 的 action URL
-    formAction: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeNBnd-yVJ7_-tMq5xaQrvt0j18UtabCFBTM0Eu2O3ivDecuQ/formResponse',
+    formAction: 'https://docs.google.com/forms/d/e/1FAIpQLSdjqlBsYS1fm1fPim3-OTC7FC7IRyCIVBSY0I80QRcSu0F7SA/viewform?usp=sf_link',
     
     // Google Form 各個 input 的 name
     input: {
-      id: 'entry.1815052017',
-      name: 'entry.1543576845',
-      gender: 'entry.892230025',
-      phone: 'entry.1003128242',
-      site: 'entry.260985931',
-      msg: 'entry.1782841550'
+      id: 'entry.143599827',
+      name: 'entry.34207673',
+      gender: 'entry.1813787574',
+      phone: 'entry.1164978269',
+      site: 'entry.335294457',
+      msg: 'entry.345524945'
     },
 
     // loading 效果要不要顯示
     loading: false
   },
   methods: {
-    // ID 限填 4 碼
+    // ID 限填 5 碼
     limitIdLen(val) {
-      if(val.length > 4) {
-        return this.id =  this.id.slice(0, 4);
+      if(val.length > 5) {
+        return this.id =  this.id.slice(0, 5);
       }
     },
     // 送出表單
@@ -53,8 +53,8 @@ const FormAutoFill = new Vue({
   },
   watch: {
     id: function(val) {
-      // ID 輸入到 4 碼就查詢資料
-      if(val.length === 4) {
+      // ID 輸入到 5 碼就查詢資料
+      if(val.length === 5) {
 
         // this.persons 裡沒這筆資料，才 POST
         if(this.persons[this.id] === undefined) {
